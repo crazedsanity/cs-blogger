c75cf4dfefdf3dbd74195fd1472154efdd5efbd8
crazedsanity <crazedsanity@6461383c-9934-45fd-8aeb-53e0b5f7c8a2>
2008-10-27 18:12:19 +0000

    Modifications (code broken) indicating changes needed for making it db-free.

--- branches/filebased_nodb/abstract/blog.abstract.class.php
+++ branches/filebased_nodb/abstract/blog.abstract.class.php
@@ -55,34 +55,8 @@ abstract class blogAbstract {
 	
 	
 	//-------------------------------------------------------------------------
-	protected function run_sql($sql) {
-		$numrows = $this->dbObj->exec($sql);
-		$dberror = $this->dbObj->errorMsg();
-		
-		if(strlen($dberror) || !is_numeric($numrows) || $numrows < 0) {
-			throw new exception(__METHOD__ .": invalid numrows (". $numrows .") or database error: ". $dberror ."<BR>\nSQL: ". $sql);
-		}
-		else {
-			$retval = $numrows;
-		}
-		
-		return($retval);
-	}//end run_sql()
-	//-------------------------------------------------------------------------
-	
-	
-	
-	//-------------------------------------------------------------------------
 	public function check_blog_exists(array $criteria) {
-		$sql = "SELECT * FROM cs_blog_table WHERE "
-			. $this->gfObj->string_from_array($criteria, 'select', NULL, 'sql');
-		$numrows = $this->run_sql($sql);
-		
-		$retval = TRUE;
-		if($numrows == 0) {
-			$retval = FALSE;
-		}
-		
+		//TODO: check if this blog directory exists...
 		return($retval);
 	}//end check_blog_exists()
 	//-------------------------------------------------------------------------
@@ -91,16 +65,8 @@ abstract class blogAbstract {
 	
 	//-------------------------------------------------------------------------
 	public function get_id_from_name($blogName) {
-		$sql = "SELECT * FROM cs_blog_table WHERE blog_name='". $blogName ."'";
-		$numrows = $this->run_sql($sql);
 		
-		if($numrows == 1) {
-			$data = $this->dbObj->farray_fieldnames();
-			$retval = $data['blog_id'];
-		}
-		else {
-			throw new exception(__METHOD__ .": failed to record for (". $blogName .")");
-		}
+		//TODO: find out what this is used for...?
 		
 		return($retval);
 	}//end get_id_from_name()
@@ -111,25 +77,7 @@ abstract class blogAbstract {
 	//-------------------------------------------------------------------------
 	public function can_access_blog($blogName, $username=NULL) {
 		if(!is_null($username) && strlen($username)) {
-			$uid = $username;
-			if(strlen($username) && !is_numeric($username)) {
-				$uid = $this->get_my_uid($username);
-			}
-			
-			$sql = "SELECT * FROM cs_blog_access_table WHERE uid=". $uid 
-					." AND blog_id=". $this->get_id_from_name($blogName);
-			$numrows = $this->run_sql($sql);
-			
-			if($numrows == 1) {
-				$retval = TRUE;
-			}
-			elseif($numrows > 1) {
-				throw new exception(__METHOD__ .": multiple records (". $numrows .") returned " .
-						"for blogName=(". $blogName .") and username=(". $username .")");
-			}
-			else {
-				$retval = FALSE;
-			}
+			//TODO: access a file to determine this.
 		}
 		else {
 			$retval = FALSE;
@@ -278,20 +226,8 @@ abstract class blogAbstract {
 	
 	//-------------------------------------------------------------------------
 	protected function get_blogs_for_location($location=NULL) {
-		$sql = "SELECT * FROM cs_blog_table";
-		if(!is_null($location) && strlen($location)) {
-			$sql .= " WHERE location = '". $location ."'";
-		}
-		$sql .= " ORDER BY blog_id";
-		$numrows = $this->run_sql($sql);
+		//TODO: determine filesystem equivalent for this...
 		
-		if($numrows > 0) {
-			$data = $this->dbObj->farray_nvp('blog_id', 'blog_name');
-			$this->validBlogs = $data;
-		}
-		else {
-			throw new exception(__METHOD__ .": failed to retrieve any blogs for location (". $location .")");
-		}
 		return($data);
 	}//end get_blogs_for_location()
 	//-------------------------------------------------------------------------
@@ -300,18 +236,8 @@ abstract class blogAbstract {
 	
 	//-------------------------------------------------------------------------
 	protected function get_blog_data(array $criteria) {
-		$sql = "SELECT a.username, b.* FROM cs_blog_table AS b INNER JOIN " .
-				"cs_authentication_table AS a ON (a.uid=b.uid) WHERE ";
-		$sql .= $this->gfObj->string_from_array($criteria, 'select', NULL, 'sql');
 		
-		$numrows = $this->run_sql($sql);
-		
-		if($numrows >= 1) {
-			$retval = $this->fix_db_array_indexes($this->dbObj->farray_fieldnames('b.blog_name', NULL, FALSE));
-		}
-		else {
-			throw new exception(__METHOD__ .": failed to retrieve data, SQL::: ". $sql);
-		}
+		//TODO: determine filesystem equivalent for this...
 		
 		return($retval);
 	}//end get_blog_data()
@@ -336,35 +262,7 @@ abstract class blogAbstract {
 	public function get_entry($nameOrId) {
 		$retval = NULL;
 		
-		if(is_numeric($nameOrId)) {
-			$criteria = "blog_entry_id=". $nameOrId;
-		}
-		elseif(is_string($nameOrId) && strlen($nameOrId)) {
-			if(!is_numeric($this->blogId)) {
-				throw new exception(__METHOD__ .": invalid blogId (". $this->blogId .")");
-			}
-			$criteria = "be.blog_id=". $this->blogId ." AND permalink='". $this->blogName .'/'. $nameOrId ."'";
-		}
-		else {
-			throw new exception(__METHOD__ .": invalid criteria (". $nameOrId .")");
-		}
-		$sql = "SELECT be.*, b.blog_name, a.username FROM cs_blog_entry_table AS be INNER JOIN " .
-				"cs_blog_table AS b ON (b.blog_id=be.blog_id) INNER JOIN " .
-				"cs_authentication_table AS a ON (a.uid=b.uid) WHERE ". 
-				$criteria ." ORDER BY post_timestamp " .
-				"DESC LIMIT 1";
-		$numrows = $this->run_sql($sql);
-		
-		if($numrows == 1) {
-			$retval = $this->fix_db_array_indexes($this->dbObj->farray_fieldnames());
-			$retval['formatted_username'] = ucwords($retval['username']);
-			$retval['formatted_post_timestamp'] = strftime('%Y/%m/%d %I:%M %p', $retval['post_timestamp']);
-			$retval['formatted_post_timestamp_time'] = strftime('%I:%M %p', $retval['post_timestamp']);
-			$retval['contents'] = $this->fsObj->read($retval['filename']);
-		}
-		else {
-			throw new exception(__METHOD__ .": failed to retrieve data for entry (". $nameOrId ."), numrows=(". $numrows .")<BR>\nSQL::: ". $sql);
-		}
+		//TODO: determine filesystem equivalent...
 		
 		return($retval);
 	}//end find_entry()
@@ -374,41 +272,8 @@ abstract class blogAbstract {
 	
 	//-------------------------------------------------------------------------
 	public function get_my_uid($uidOrUsername=NULL, $setInternal=FALSE) {
-		if(!is_null($uidOrUsername) && strlen($uidOrUsername)) {
-			if(is_numeric($uidOrUsername)) {
-				$criteria = "uid=". $uidOrUsername;
-			}
-			elseif(is_string($uidOrUsername) && !is_numeric($uidOrUsername) && strlen($uidOrUsername)) {
-				$criteria = "username='". $uidOrUsername ."'";
-			}
-			else {
-				cs_debug_backtrace(1);
-				throw new exception(__METHOD__ .": unknown data in uid or username (". $uidOrUsername .")");
-			}
-			$sql = "SELECT * FROM cs_authentication_table WHERE ". $criteria;
-			$retval = $this->run_sql($sql);
-			
-			if($retval == 1) {
-				$data = $this->dbObj->farray_fieldnames();
-				$retval = $data['uid'];
-				if($setInternal) {
-					$this->uid = $retval;
-				}
-				
-				if(!is_numeric($retval)) {
-					cs_debug_backtrace(1);
-					throw new exception(__METHOD__ .": couldn't find uid in data array::: ". $this->gfObj->debug_print($data,0),1);
-				}
-			}
-			else {
-				cs_debug_backtrace(1);
-				throw new exception(__METHOD__ .": failed to retrieve uid for (". $criteria .")");
-			}
-		}
-		else {
-			cs_debug_backtrace(1);
-			throw new exception(__METHOD__ .": no uid or username given (". $uidOrUsername .")");
-		}
+		
+		//TODO: use filesystem equivalent of this...
 		
 		return($retval);
 	}//end get_my_uid()
--- branches/filebased_nodb/abstract/editBlog.abstract.class.php
+++ branches/filebased_nodb/abstract/editBlog.abstract.class.php
@@ -2,6 +2,9 @@
 
 require_once(dirname(__FILE__) .'/blog.abstract.class.php');
 
+
+//TODO: determine if there's a difference between permalink & filename; only use one or the other(?).
+
 abstract class editBlogAbstract extends blogAbstract {
 	
 	#protected $dbObj;
@@ -27,47 +30,7 @@ abstract class editBlogAbstract extends blogAbstract {
 	//-------------------------------------------------------------------------
 	public function create_new_blog($blogName, $username, $location=NULL) {
 		
-		$this->get_my_uid($username, TRUE);
-		
-		if(is_null($location) || !strlen($location)) {
-			$location = $this->defaultLocation;
-		}
-		
-		$retval = FALSE;
-		if(!$this->check_blog_exists(array('blog_name'=>$blogName))) {
-			if(!is_numeric($this->uid)) {
-				throw new exception(__METHOD__ .": invalid uid (". $this->uid .")");
-			}
-			$insertArr = array(
-				'uid'		=> $this->uid,
-				'blog_name'=> $blogName,
-				'location'	=> $location
-			);
-			$sql = 'INSERT INTO cs_blog_table '. $this->gfObj->string_from_array($insertArr, 'insert', NULL, 'sql92_insert');
-			$numrows = $this->run_sql($sql);
-			
-			if($numrows == 1) {
-				//ok, give some permissions and return the blog_id.
-				$retval = $this->dbObj->lastOID();
-				$this->blogId = $retval;
-				
-				if(!is_numeric($retval)) {
-					throw new exception(__METHOD__ .": failed to retrieve last inserted record (". $retval .")");
-				}
-				
-				$insertArr = array(
-					'blog_id'	=> $retval,
-					'uid'		=> $this->uid
-				);
-				$this->add_user_to_blog($blogName, $username);
-			}
-			else {
-				throw new exception(__METHOD__ .": invalid number of rows returned (". $numrows .")");
-			}
-		}
-		else {
-			$retval = $this->get_id_from_name($blogName);
-		}
+		//TODO: this should just create a new blog directory with proper permissions.
 		
 		return($retval);
 	}//end create_new_blog()
@@ -85,19 +48,7 @@ abstract class editBlogAbstract extends blogAbstract {
 			$retval = TRUE;
 		}
 		else {
-			$insertArr = array(
-				'blog_id'	=> $this->get_id_from_name($blogName),
-				'uid'		=> $this->get_my_uid($username)
-			);
-			$sql = 'INSERT INTO cs_blog_access_table '. $this->gfObj->string_from_array($insertArr, 'insert', NULL, 'sql92_insert');
-			$numrows = $this->run_sql($sql);
-			
-			if($numrows == 1) {
-				$retval = TRUE;
-			}
-			else {
-				throw new exception(__METHOD__ .": invalid return from sql (". $numrows .")...");
-			}
+			//TODO: modify file that specifies user access to blogs.
 		}
 		
 		return($retval);
@@ -108,8 +59,13 @@ abstract class editBlogAbstract extends blogAbstract {
 	
 	//-------------------------------------------------------------------------
 	public function add_entry($blogId, $title, $contents, $postDate=NULL) {
-		//TODO: this should create the blog file!!!
+		
+		
+		//TODO: don't worry about "blogId" (should be "blogName")
+		
+		
 		if(!is_numeric($this->uid)) {
+			//TODO: is uid really required, or just a leftover from the database days?
 			throw new exception(__METHOD__ .": no uid set");
 		}
 		
@@ -125,35 +81,14 @@ abstract class editBlogAbstract extends blogAbstract {
 			$postDate = $obj->format('U');
 		}
 		
-		$this->blogId = $blogId;
-		
 		$filename = $this->filename_from_title($title);
 		
-		$insertArr = array(
-			'blog_id'			=> $blogId,
-			'author_uid'		=> $this->uid,
-			'filename'			=> $filename,
-			'title'				=> $title,
-			'post_timestamp'	=> $postDate,
-			'create_date'		=> time(),
-			'permalink'		=> $this->get_permalink_from_filename($filename)
-		);
-		$cleanStringArr = array(
-			'blog_id'			=> 'integer',
-			'author_uid'		=> 'integer',
-			'filename'			=> 'sql92_insert',
-			'title'				=> 'sql92_insert',
-			'post_timestamp'	=> 'integer',
-			'create_date'		=> 'integer',
-			'permalink'		=> 'sql92_insert'
-		);
-		
-		$permalinkExists = $this->permalink_exists($insertArr['permalink']);
+		$permalinkExists = $this->permalink_exists($this->get_permalink_from_filename($filename));
 		if($permalinkExists) {
 			if($this->allow_dup_entries()) {
 				//add the suffix.
-				$insertArr['filename'] = $this->filename_from_title($title,$permalinkExists);
-				$insertArr['permalink'] = $this->get_permalink_from_filename($insertArr['filename']);
+				$filename = $this->filename_from_title($title,$permalinkExists);
+				$insertArr['permalink'] = $this->get_permalink_from_filename($filename);
 			}
 			else {
 				//no dups allowed, and the entry already exists.
@@ -162,29 +97,13 @@ abstract class editBlogAbstract extends blogAbstract {
 		}
 		
 		//create the filename; if that works, do the insert.
-		$myFilename = $insertArr['filename'];
-		$createFileRes = $this->fsObj->create_file($myFilename);
+		$createFileRes = $this->fsObj->create_file($filename);
 		if($createFileRes) {
 			
 			//now write the file's contents.
-			$this->fsObj->openFile($myFilename, 'w+');
-			$writeFile = $this->fsObj->write($contents);
+			$this->fsObj->openFile($filename, 'w+');
+			$retval = $this->fsObj->write($contents);
 			
-			if($writeFile) {
-				$sql = 'INSERT INTO cs_blog_entry_table '. $this->gfObj->string_from_array($insertArr, 'insert', NULL, $cleanStringArr);
-				$numrows = $this->run_sql($sql);
-				
-				if($numrows == 1) {
-					//get the last inserted blog_entry_id (yeah, sorry it's confusing).
-					$retval = $this->dbObj->lastOID();
-				}
-				else {
-					throw new exception(__METHOD__ .": record unable to be inserted (". $numrows .")");
-				}
-			}
-			else {
-				throw new exception(__METHOD__ .": failed to write file contents (". $writeFile .") after creating it (". $createFileRes .")");
-			}
 		}
 		else {
 			throw new exception(__METHOD__ .": failed to create file (". $createFileRes .")");
@@ -221,36 +140,8 @@ abstract class editBlogAbstract extends blogAbstract {
 			if(preg_match('/\'/', $permalink)) {
 				throw new exception(__METHOD__ .": permalink (". $permalink .") contains single quotes");
 			}
-			$sql = "SELECT * FROM cs_blog_entry_table WHERE blog_id='". $this->blogId 
-					."' AND permalink='". $permalink ."'";
-			$numrows = $this->run_sql($sql);
 			
-			if($numrows == 1) {
-				//check for more blogs...?
-				$sql = "SELECT * FROM cs_blog_entry_table WHERE blog_id=". $this->blogId .
-					" AND permalink like '". $permalink ."-%' ORDER BY blog_entry_id" .
-					" DESC LIMIT 1";
-					
-				$numrows = $this->run_sql($sql);
-				
-				if($numrows == 1) {
-					//determine the number for the next dup.
-					$data = $this->dbObj->farray_fieldnames();
-					$pieces=explode('-', $data['permalink']);
-					
-					$lastNum = $pieces[1];
-					$retval = $lastNum +1;
-				}
-				else {
-					$retval = 1;
-				}
-			}
-			elseif($numrows == 0) {
-				$retval = 0;
-			}
-			else {
-				throw new exception(__METHOD__ .": invalid numrows returned (". $numrows .")");
-			}
+			//TODO: look through the current blog (currently "blogId") to see if that permalink/filename exists.
 		}
 		else {
 			throw new exception(__METHOD__ .": invalid permalink given (". $permalink .")");
@@ -264,44 +155,10 @@ abstract class editBlogAbstract extends blogAbstract {
 	
 	//-------------------------------------------------------------------------
 	public function update_entry($entryId, array $changes=NULL, $contents=NULL) {
+		//TODO: stop referring to entryId; instead, refer to the filename.
 		if(isset($entryId) && is_numeric($entryId)) {
-			$totalChanges = 0;
-			$retval = 0;
-			$this->dbObj->beginTrans();
-			$myData = $this->get_entry($entryId);
-			if(is_array($changes) && count($changes)) {
-				$totalChanges++;
-				if(isset($changes['post_timestamp'])) {
-					$changes['post_timestamp'] = strtotime($changes['post_timestamp']);
-				}
-				if($changes['post_timestamp'] > time()) {
-					//TODO: consider allowing this, and only showing logs with past timestamps (while showing users future-dated entries).
-					$changes['post_timestamp'] = time();
-				}
-				$sql = 'UPDATE cs_blog_entry_table SET '. $this->gfObj->string_from_array($changes, 'update', NULL, 'sql92_insert')
-						. ' WHERE blog_entry_id='. $entryId;
-				$numrows = $this->run_sql($sql);
-				if($numrows == 1) {
-					$retval++;
-				}
-				else {
-					throw new exception(__METHOD__ .": failed to update entryId=(". $entryId .") with result: (". $retval .")");
-				}
-			}
-			if(!is_null($contents) && strlen($contents)) {
-				$totalChanges++;
-				if($this->fsObj->truncate_file($myData['filename'])) {
-					$writeRes = $this->fsObj->write(stripslashes($contents), $myData['filename']);
-				}
-				else {
-					throw new exception(__METHOD__ .": failed to truncate file");
-				}
-				$retval ++;
-			}
-			$this->dbObj->commitTrans();
-			if($totalChanges < 1) {
-				throw new exception(__METHOD__ .": no changes given");
-			}
+			//TODO: use this to update the file.
+			//TODO: update any indices that might be affected.
 		}
 		else {
 			throw new exception(__METHOD__ .": invalid entryId (". $entryId .")");
