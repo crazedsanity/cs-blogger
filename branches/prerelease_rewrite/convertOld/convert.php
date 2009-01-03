7a3d6d9c37a991e3ad100943a3ce4d61fb3861fd
crazedsanity <crazedsanity@6461383c-9934-45fd-8aeb-53e0b5f7c8a2>
2009-01-03 18:15:08 +0000

    Beginning of the conversion script.

--- branches/prerelease_rewrite/convertOld/convert.php
+++ branches/prerelease_rewrite/convertOld/convert.php
@@ -5,4 +5,58 @@
  * number, where it used filenames as the entry text).
  */
 
+require_once(dirname(__FILE__) .'/../../siteConfig.php');
+require_once(dirname(__FILE__) .'/../../cs-content/cs_phpDB.php');
+require_once(dirname(__FILE__) .'/../../cs-content/cs_globalFunctions.php');
+require_once(dirname(__FILE__) .'/../../cs-content/cs_fileSystemClass.php');
+
+
+
+$oldDbParams = array(
+	'dbname'	=> CS_BLOGDBNAME,
+	'rwDir'		=> CS_SQLITEDBDIR
+	
+);
+$newDbParams = array(
+	'host'		=> "localhost",
+	'port'		=> 5432,
+	'dbname'	=> "cs",
+	'user'		=> "postgres",
+	'password'	=> ""
+);
+
+
+$obj = new converter($newDbParams, $oldDbParams);
+
+class converter {
+	
+	/** Connection to the old SQLite database */
+	private $oldDb;
+	
+	/** Connection to the new PostgreSQL database */
+	private $newDb;
+	
+	/** Object to interact with the filesystem. */
+	private $fsObj;
+	
+	/** cs_globalFunctions{} object. */
+	private $gfObj;
+	
+	
+	//-------------------------------------------------------------------------
+	public function __construct($newDbParms, $oldDbParms) {
+		$this->oldDb = new cs_phpDB('sqlite');
+		$this->oldDb->connect($oldDbParms);
+		
+		$this->newDb = new cs_phpDB('pgsql');
+		$this->newDb->connect($newDbParms);
+		
+		$this->fsObj = new cs_fileSystemClass($oldDbParms['rwDir']);
+		
+		$this->gfObj = new cs_globalFunctions;
+		$this->gfObj->debugPrintOpt = 1;
+	}//end __construct()
+	//-------------------------------------------------------------------------
+	
+}//end converter{}
 ?>
