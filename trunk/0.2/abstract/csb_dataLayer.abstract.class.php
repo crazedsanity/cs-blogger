86c93a7af443bec8a205be7bcd8af4090c26785f
crazedsanity <crazedsanity@6461383c-9934-45fd-8aeb-53e0b5f7c8a2>
2009-08-22 22:38:56 +0000

    Fix location of cs_phpDB{}.

--- trunk/0.2/abstract/csb_dataLayer.abstract.class.php
+++ trunk/0.2/abstract/csb_dataLayer.abstract.class.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(dirname(__FILE__) .'/../../cs-content/cs_phpDB.class.php');
+require_once(dirname(__FILE__) .'/../../cs-webapplibs/cs_phpDB.class.php');
 require_once(dirname(__FILE__) .'/../../cs-content/cs_globalFunctions.class.php');
 require_once(dirname(__FILE__) .'/../../cs-content/abstract/cs_version.abstract.class.php');
 require_once(dirname(__FILE__) .'/../csb_location.class.php');
