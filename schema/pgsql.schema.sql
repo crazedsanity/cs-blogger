98a297d5add06b757e4ec385593f81c50bcd83fd
Dan Falconer <github@crazedsanity.com>
2013-02-23 02:04:01 -0600

    Cleaner schema
    Removed unnecessary tables & such (provided by other systems).

--- schema/pgsql.schema.sql
+++ schema/pgsql.schema.sql
@@ -1,15 +1,6 @@
 
 --begin;
 
-CREATE TABLE cs_authentication_table (
-	uid serial NOT NULL PRIMARY KEY,
-	username text NOT NULL UNIQUE,
-	passwd varchar(32),
-	is_active boolean DEFAULT true NOT NULL,
-	date_created date DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	last_login timestamp with time zone,
-	email text
-);
 CREATE TABLE csblog_location_table (
 	location_id serial NOT NULL PRIMARY KEY,
 	location text NOT NULL UNIQUE
@@ -39,16 +30,6 @@ CREATE TABLE csblog_entry_table (
 CREATE UNIQUE INDEX csblog_entry_table_permalink_blog_id_uidx ON csblog_entry_table USING btree (permalink,blog_id);
 
 
-CREATE TABLE cs_session_table (
-	session_id character varying(32) NOT NULL PRIMARY KEY,
-	uid integer NOT NULL REFERENCES cs_authentication_table(uid),
-	create_date timestamp NOT NULL,
-	last_checkin timestamp,
-	num_checkins  integer NOT NULL DEFAULT 0,
-	ip varchar(15) NOT NULL
-);
-
---
 -- Table for storing basic permissions.
 --
 CREATE TABLE csblog_permission_table (
@@ -77,10 +58,4 @@ CREATE TABLE csblog_comment_table (
 
 
 
--- Add entries for users.
---NOTE: password for "test" is "test".
-INSERT INTO cs_authentication_table (username, passwd) VALUES('test','a0d987ef6826c00ff6e4ac0851ea4744');
-
--- SET NEXT UID TO BE > 100...
-select setval('cs_authentication_table_uid_seq',100);
 --abort;
